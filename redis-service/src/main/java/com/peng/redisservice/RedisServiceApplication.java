ab854577791bdd31937debf21988cbcc49c09542
pengxy <pengxuanyus@gmail.com>
2021-02-28 22:58:58 +0800

    springcloud redis-service

---
+++ redis-service/src/main/java/com/peng/redisservice/RedisServiceApplication.java
@@ -0,0 +1,40 @@
+package com.peng.redisservice;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.support.collections.RedisCollectionFactoryBean;
+
+@SpringBootApplication
+public class RedisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisServiceApplication.class, args);
+	}
+
+
+	@Bean
+	public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+		RedisTemplate<Object,Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
+		@SuppressWarnings("unchecked")
+		Jackson2JsonRedisSerializer jsonRedisSerializer	= new Jackson2JsonRedisSerializer(Object.class);
+		ObjectMapper om = new ObjectMapper();
+		om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+		om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+		jsonRedisSerializer.setObjectMapper(om);
+
+		redisTemplate.setValueSerializer(jsonRedisSerializer);
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+		return redisTemplate;
+	}
+
+}

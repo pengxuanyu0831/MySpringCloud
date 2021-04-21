f6f27dc2473d00fc5bc0313b3aeb6d19d066bf46
pengxy <pengxuanyus@gmail.com>
2021-04-21 22:24:05 +0800

    读取配置文件

--- base/src/main/java/com/peng/base/controller/Usercontroller.java
+++ base/src/main/java/com/peng/base/controller/Usercontroller.java
@@ -4,14 +4,19 @@ import com.peng.base.mapper.UserMapper;
 import com.peng.base.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * @program MySpringCloud
@@ -36,5 +41,24 @@ public class Usercontroller {
         list.add("2");
         //return userMapper.seletByUserId("1");
         return list;
+    }
+
+
+    //@GetMapping("/fileOpen")
+    public static void  fileOpen(){
+        Properties properties = new Properties();
+        try{
+            properties = PropertiesLoaderUtils.loadAllProperties("question.properties");
+            for(Object key:properties.entrySet()){
+                System.out.println(key);
+                System.out.println(properties.get(key));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        fileOpen();
     }
 }
